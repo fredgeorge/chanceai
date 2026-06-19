3f90bac06e01cd68808bfe5506c1ea378d192f29
Fred George <fredgeorge@acm.org>
2026-06-19 08:28:54 +0200

    Qwen 3.6 80B: 3 passes with some test tuning required

--- engine/src/main/kotlin/com/nrkei/project/chanceai/Chance.kt
+++ engine/src/main/kotlin/com/nrkei/project/chanceai/Chance.kt
@@ -1,34 +1,45 @@
 package com.nrkei.project.chanceai
 
 import kotlin.math.abs
-import kotlin.math.roundToLong
+import kotlin.math.pow
+import kotlin.math.round
 
-class Chance(probability: Double) {
+data class Chance(val probability: Double) {
+
     init {
-        require(probability in 0.0..1.0) { "Probability must be between 0.0 and 1.0 inclusive" }
-    }
-
-    private val probability = probability
-
-    companion object {
-        private const val EPSILON = 1e-9
-        val IMPOSSIBLE = Chance(0.0)
-        val CERTAIN = Chance(1.0)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Chance) return false
-        return abs(this.probability - other.probability) < EPSILON
-    }
-
-    override fun hashCode(): Int {
-        return (probability / EPSILON).roundToLong().hashCode()
+        require(probability in 0.0..1.0) { "Probability must be between 0.0 and 1.0, inclusive" }
     }
 
     operator fun not(): Chance = Chance(1.0 - probability)
 
     infix fun and(other: Chance): Chance = Chance(this.probability * other.probability)
 
-    infix fun or(other: Chance): Chance = !((!this) and (!other))
+    infix fun or(other: Chance): Chance = !(this.not() and other.not())
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Chance) return false
+        return abs(probability - other.probability) < EPSILON
+    }
+
+    override fun hashCode(): Int {
+        val adjusted = roundToPrecision(PRECISION)
+        return adjusted.hashCode()
+    }
+
+    companion object {
+        val IMPOSSIBLE = Chance(0.0)
+        val CERTAIN = Chance(1.0)
+
+        private const val EPSILON = 1e-9
+        private const val PRECISION = 9
+    }
+
+    private fun roundToPrecision(precision: Int): Double {
+        return round(this.probability * kpow(10, precision)) / kpow(10, precision)
+    }
+
+    private fun kpow(base: Int, exp: Int): Double {
+        return base.toDouble().pow(exp.toDouble())
+    }
 }
